7d8a4f6b55dafb4cb04b250e2c0ac6095073e9d6
tang0226 <58008827+tang0226@users.noreply.github.com>
2025-07-10 21:33:12 -0700

    Add movement and game-end checking to _2048 engine

--- js/games/2048.js
+++ js/games/2048.js
@@ -9,22 +9,19 @@ class _2048 {
       this.grid.push(new Array(this.gridSize));
     }
 
-    this.score = 0;
-    this.turns = 0;
-
     this.reset();
   }
 
   reset() {
     this.score = 0;
     this.turns = 0;
-    
+    this.gameOver = false;
+
     for (let row of this.grid) {
       row.fill(0);
     }
     this.randomSpawn();
     this.randomSpawn();
-    console.log(this.grid);
   }
 
   randomSpawn() {
@@ -46,10 +43,130 @@ class _2048 {
     }
   }
 
+  move(dir) {
+    let turnScore = 0, ortho, orthoDir;
+
+    switch (dir) {
+      case this.constructor.UP:
+        ortho = 1;
+        orthoDir = -1;
+        break;
+      case this.constructor.DOWN:
+        ortho = 1;
+        orthoDir = 1;
+        break;
+      case this.constructor.RIGHT:
+        ortho = 0;
+        orthoDir = 1;
+        break;
+      case this.constructor.LEFT:
+        ortho = 0;
+        orthoDir = -1;
+        break;
+    }
+
+    let lines = [];
+    for (let i = 0; i < this.gridSize; i++) {
+      let line = [];
+      if (ortho == 1) {
+        for (let j = 0; j < this.gridSize; j++) {
+          line.push([i, j]);
+        }
+      }
+      else {
+        for (let j = 0; j < this.gridSize; j++) {
+          line.push([j, i]);
+        }
+      }
+      if (orthoDir == 1) {
+        line.reverse();
+      }
+      lines.push(line);
+    }
+
+    let action = false;
+    for (let i = 0; i < this.gridSize; i++) {
+      let posI = 0;
+      let line = lines[i]
+      let lastUncombinedTile = -1;
+      for (let j = 0; j < this.gridSize; j++) {
+        let c = line[j]
+        let tile = this.grid[c[1]][c[0]];
+        if (tile) {
+          // If the two tiles match;
+          if (tile == lastUncombinedTile) {
+            // Combine
+            this.grid[line[posI - 1][1]][line[posI - 1][0]] = tile + 1;
+            this.grid[c[1]][c[0]] = 0;
+            turnScore += this.constructor.POW2[tile + 1];
+
+            // Reset the combining
+            lastUncombinedTile = -1;
+            action = true;
+          }
+          else {
+            lastUncombinedTile = tile;
+            // If tile is already in the next position, it can't move
+            if (j != posI) {
+              // Move the tile
+              this.grid[c[1]][c[0]] = 0;
+              this.grid[line[posI][1]][line[posI][0]] = tile;
+              action = true;
+            }
+            posI++;
+          }
+        }
+      }
+    }
+    if (!action) {
+      return -1;
+    }
+
+    // Spawn another tile
+    this.randomSpawn();
+    
+    for (let i of this.grid) {
+      console.log(i);
+    }
+
+    this.score += turnScore;
+    this.turns++;
+
+    if (this.checkGameEnd()) {
+      this.gameOver = true;
+    }
+
+    return turnScore;
+  }
+
+  checkGameEnd() {
+    for (let y = 0; y < this.gridSize; y++) {
+      for (let x = 0; x < this.gridSize; x++) {
+        if (this.grid[y][x] == 0) {
+          return false;
+        }
+      }
+    }
+
+    for (let i = 0; i < this.gridSize; i++) {
+      for (let j = 0; j < this.gridSize - 1; j++) {
+        if (
+          this.grid[i][j] == this.grid[i][j + 1] ||
+          this.grid[j][i] == this.grid[j + 1][i]
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 
 
   static UP = 1;
   static RIGHT = 2;
   static DOWN = 3;
   static LEFT = 4;
+
+  static POW2 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216];
 }
--- js/main.js
+++ js/main.js
@@ -30,3 +30,42 @@ console.log(nn);
 console.log(nn.feedForward([2, 3, 4, 5]));
 
 var game = new _2048();
+
+var tileMargin = 10;
+var tileSize = 100;
+var fontSize = tileSize * 0.5;
+var colors = {
+  boardBorder: "#b9ae9f",
+  tiles: [
+    "#cbc0b3", // empty
+    "#ece4da", // 2
+    "#ebe1ca", // 4
+    "#eab477", // 8
+    "#ea9762", // 16
+    "#e87b5f", // 32
+    "#e85e3d", // 64
+    "#e6d36a", // 128
+    "#e7d058", // 256
+    "#e6cd42", // 512
+    "#e5c92b", // 1024
+    "#e4c505", // 2048
+    "#b182b6", // 4096
+    "#a259ae", // 8192
+    "#9a3ca7", // 16384
+    "#790084", // 32768
+    "#5a0048", // 65536
+    "#8f7ee7", // 131072
+  ],
+};
+
+
+var _canvasSideLength = tileSize * game.gridSize + tileMargin * (game.gridSize + 1);
+setCanvasDim(_canvasSideLength, _canvasSideLength);
+
+
+
+
+function up() {game.move(_2048.UP)}
+function down() {game.move(_2048.DOWN)}
+function right() {game.move(_2048.RIGHT)}
+function left() {game.move(_2048.LEFT)}
