1337517e234fc7e2fd2a331c65980cca3603bea0
tang0226 <58008827+tang0226@users.noreply.github.com>
2025-07-19 22:49:42 -0700

    Update file structure and add image approximation folder

---
+++ experiments/image-approximation/main.js
@@ -0,0 +1,14 @@
+const mainCanvas = document.getElementById("main-canvas");
+const ctx = mainCanvas.getContext("2d");
+
+var width, height;
+function setCanvasDim(w, h) {
+  mainCanvas.width = w;
+  mainCanvas.height = h;
+  mainCanvas.style.width = w + "px";
+  mainCanvas.style.height = h + "px";
+  width = w;
+  height = h;
+}
+
+setCanvasDim(1000, 1000);
